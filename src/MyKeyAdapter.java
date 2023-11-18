929b0612a87a5ce53b55c3485f34dff1971942b8
bijanghanei <iambijanghanei@gmail.com>
2023-11-18 10:54:48 -0500

    MyKeyAdapter added

--- src/MyKeyAdapter.java
+++ src/MyKeyAdapter.java
@@ -2,10 +2,39 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class MyKeyAdapter extends KeyAdapter {
-
-    public void KeyPressed(KeyEvent e) {
-
-
+    char direction;
+    MyKeyAdapter(char d){
+        this.direction = d;
     }
-}
 
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R'){
+                        direction = 'L';
+                        System.out.println(direction);
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L'){
+                        direction = 'R';
+                        System.out.println(direction);
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U'){
+                        direction = 'D';
+                        System.out.println(direction);
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D'){
+                        direction = 'U';
+                        System.out.println(direction);
+                    }
+                    break;
+            }
+       }
+   }
